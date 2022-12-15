d95cbf2926caf965344010b1b8879865d79f71d0
AnushaYadaV4 <anu468mjl@gmail.com>
2022-12-15 14:49:26 +0530

    eleventh commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useCallback, useMemo } from 'react';
+import DemoList from './components/Demo/DemoList';
+import Button from './UI/Button/Button';
+
+import './App.css';
+
+function App() {
+  const [listTitle, setListTitle] = useState('My List');
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
+  const changeNewTitleHandler = useCallback(() => {
+    setListTitle('Change to Ascending Order');
+    
+  }
+  )
+    
+
+
+  
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+ 
+  return (
+    <div className="app">
+       <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+
+      <Button onClick={changeNewTitleHandler}>change to descending order</Button>
+
+    </div>
+
+
+
+  );
+}
+
+export default App;
